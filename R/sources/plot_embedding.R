2cff60076ee0f418e897b1ad3fe8c1b493714301
Fan Cheng <Fan.Cheng@monash.edu>
2021-06-09 23:39:40 +1000

    plot.embedding() function for metricmL() output

---
+++ R/sources/plot_embedding.R
@@ -0,0 +1,23 @@
+# Embedding plot of metricML() output for electricity data
+plot.embedding <- function(x) {
+  fn <- x$embedding
+  
+  todcolor <- colorspace::scale_color_continuous_sequential(
+    palette = "viridis",
+    breaks = c(12, 24, 36, 48),
+    labels=c("06:00", "12:00", "18:00", "24:00"),
+    name="Time of day",
+    guide=guide_colorbar(barwidth = 10))
+  
+  p <- fn %>% 
+    cbind(tod = rep(1:48, N / 48)) %>% 
+    as_tibble() %>% 
+    ggplot(aes(x = E1, y = E2, col = tod)) + 
+    geom_point() + 
+    # labs(x = "ISO1", y = "ISO2") +
+    coord_fixed(ratio = 1) + 
+    todcolor + 
+    theme(legend.position = 'bottom')
+  
+  return(p)
+}
