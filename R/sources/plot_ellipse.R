a7fda31f96f6d66c144cf980da37356fc9141fbd
Fan Cheng <Fan.Cheng@monash.edu>
2021-06-17 10:23:52 +1000

    Plotting done with metricML output

--- R/sources/plot_ellipse.R
+++ R/sources/plot_ellipse.R
@@ -1,14 +1,20 @@
-plot.ellipse <- function(x, add = FALSE, n.plot = 50, 
+plot.ellipse <- function(x, add = FALSE, n.plot = 50, scale = 20,
              color = blues9[5], fill = blues9[5], alpha = 0.2, ...){
   
-  p <- geom_ellipse(data = slice_sample(x, n = n.plot), 
+  fn <- x$embedding
+  Rn <- x$rmetric # array
+  e <- riem2ellipse(Rn, scale = 20) %>% 
+    cbind(fn) %>% 
+    as_tibble()
+  
+  p <- geom_ellipse(data = slice_sample(e, n = n.plot), 
                     aes(x0 = E1, y0 = E2, a = a, b = b, angle = angle, group = -1), 
                     color = color, fill = fill, alpha = alpha, ...)
   
   if(add){
     p <- p
   } else {
-    p <- plot.embedding(x, embedding = T) + p
+    p <- plot.embedding(x, embedding = F) + p
   }
  
   return(p)
