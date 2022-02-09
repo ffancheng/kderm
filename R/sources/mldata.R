fadf2dfec1604621afd06026d2b0dd2dcc5f4fc0
Fan Cheng <Fan.Cheng@monash.edu>
2022-02-09 23:59:00 +1100

    Functions for kde to be improved

--- R/sources/mldata.R
+++ R/sources/mldata.R
@@ -11,11 +11,15 @@
 #' @export
 #'
 #' @examples
+#' library(copula)
 #' a <- mldata(N = 100, meta = "copula", mapping = "Swiss Roll")
 #' library(ggplot2)
 #' ggplot(as_tibble(a$data), aes(x, y)) + geom_point(aes(color = a$colors))
 #' b <- mldata(meta = "gaussian", mapping = "S Curve")
 #' ggplot(as_tibble(b$data), aes(x, y)) + geom_point(aes(color = b$colors))
+#' dat <- as.data.frame(b$data)
+#' library(scatterplot3d)
+#' scatterplot3d(x = dat$x, y = dat$y, z = dat$z, color = b$colors)
 mldata <- function(N = 2000, p = 2, meta = c("uniform", "copula", "gaussian"), 
                    mapping = c("Swiss Roll", "semi-sphere", "Twin Peak", "S Curve")) {
   switch (meta,
@@ -31,34 +35,40 @@ mldata <- function(N = 2000, p = 2, meta = c("uniform", "copula", "gaussian"),
           },
           "gaussian" = {
             n <- round(N/4)
-            R <- matrix(c(1, 0,
-                          0, 1), 
+            R <- matrix(c(.02, 0,
+                          0, .02), # variance-covariance matrix
                         nrow = 2, ncol = 2)
-            # mu1 <- c(7.5, 7.5)
-            # mu2 <- c(7.5, 12.5)
-            # mu3 <- c(12.5, 7.5)
-            # mu4 <- c(12.5, 12.5)
-            mu <- tribble(~mu1, ~mu2,
-                          7.5, 7.5,
-                          7.5, 12.5,
-                          12.5, 7.5,
-                          12.5, 12.5)
+            # mu <- matrix(#~mu1, ~mu2,
+            #               c(7.5, 7.5,
+            #               7.5, 12.5,
+            #               12.5, 7.5,
+            #               12.5, 12.5), 
+            #               4, 2, byrow = TRUE)
+            mu <- matrix(#~mu1, ~mu2,
+              c(0.25, 0.25,
+                0.25, 0.75,
+                0.75, 0.25,
+                0.75, 0.75), 
+              4, 2, byrow = TRUE)
             co <- NULL
             for(i in 1:4) {
-              # mui <- get(paste0("mu", i))
-              mui <- as.matrix(mu[i,])
-              a <- mvtnorm::rmvnorm(n, mean = mui, sigma = R)
-              den <- mvtnorm::dmvnorm(a, mean = mui, sigma = R)
+              a <- mvtnorm::rmvnorm(n, mean = mu[i,], sigma = R)
+              # True density is the mean of densities with all four cores (same R different mus)
+              den <- cbind(mvtnorm::dmvnorm(a, mean = mu[1,], sigma = R),
+                          mvtnorm::dmvnorm(a, mean = mu[2,], sigma = R),
+                          mvtnorm::dmvnorm(a, mean = mu[3,], sigma = R),
+                          mvtnorm::dmvnorm(a, mean = mu[4,], sigma = R)
+              ) %>% rowMeans()
               a <- cbind(a, den, i)
               co <- rbind(co, a)
             }
             colnames(co) <- c("x", "y", "den", "label")
             co <- co %>%
               as_tibble() %>% 
-              mutate(# label = as.factor(label),
-                     x = (x - 5)/10, y = (y - 5)/10,
-                     den = den * 10 # y = h(x) = (x-5)/10; x = 10*y + 5; dx/dy = 10; f_Y(y) = f_X(x) * 10
-              ) %>% 
+              # mutate(# label = as.factor(label),
+              #        x = (x - 5)/10, y = (y - 5)/10,
+              #        den = den * 10 # y = h(x) = (x-5)/10; x = 10*y + 5; dx/dy = 10; f_Y(y) = f_X(x) * 10
+              # ) %>% 
               as.matrix()
           }
   )
@@ -89,6 +99,7 @@ mldata <- function(N = 2000, p = 2, meta = c("uniform", "copula", "gaussian"),
   
   return(list(data = data, metadata = u, den = co[, "den"], colors = colors))
 }
+
 ## Utility functions
 # swiss roll mapping
 swissRollMapping <- function (x, y) { # U(1.5pi, 4.5pi)
