7650850dd4da0897ba1c293103fdeccca0a4bcf5
Fan Cheng <fan.cheng@monash.edu>
2022-05-03 20:11:18 +1000

    S1 example done

--- R/riemgeom_s2.R
+++ R/riemgeom_s2.R
@@ -1,75 +1,63 @@
 rm(list= ls())
 library(tidyverse)
 library(dimRed)
-# library(viridis)
-# library(hdrcde)
 library(ks)
-# library(patchwork)
-# # library(copula)
-# library(plotly)
-# library(kableExtra)
 Jmisc::sourceAll(here::here("R/sources"))
 
 set.seed(1)
 N <- 10000
-d <- 2
+d <- 1
 theta <- runif(N, 0, pi/2)
-# theta <- rnorm(N, pi / 4, pi / 20)
 # density
 dentheta <- dunif(theta, 0, pi/2) # density = 1/(MAX-MIN) = 0.6366198
+# theta <- rnorm(N, pi / 4, pi / 20)
+# dentheta <- dnorm(theta, pi / 4, pi / 20)
+# denx <- 1 / sqrt(1 - x ^ 2) * dnorm(theta, pi / 4, pi / 20)
 x <- cos(theta)
 y <- sin(theta)
 plot(x, y, asp = 1)
 # range(x) # [0, 1]
+# True density of x
 denx <- 2 / pi * (1 / sqrt(1 - x ^ 2))
 plot(x, denx)
 range(denx)
 
-
+# KDE with fixed bandwidth
 h <- ks::hpi(x, binned = TRUE) # 0.034
 # h <- 0.05
 fxkde <- kde(x, eval.points = x)$estimate
 plot(x, fxkde)
-
+# KDE with Riemannian metric
 d <- 1
 fx <- rep(0, N)
 # eval.points <- x
 for(i in 1:N){
-  # fi <- sqrt(1 - x[i] ^ 2) * dmvnorm(x = eval.points, mean = x[i], sigma = diag(h, N))
-  # fx <- sum(fx, fi)
   fi <- 1 / sqrt(1 - x[i] ^ 2) * dnorm(x = acos(x), mean = acos(x[i]), sd = h)
   fx <- fx + fi
 }
 fx <- fx / N
 
+# Plotting
 par(mfrow=c(2,2))
-plot(x, denx)
-plot(x, fx)
-plot(x, fxkde)
-plot(x, dentheta)
+plot(x, denx, main = "True density of x")
+plot(x, fxkde, main = paste("Estimated f(x) with KDE, h = ", round(h, 3)))
+plot(x, fx, main = "Estimated f(x) with riemannian metric")
+abline(h = dentheta, col = "red", lty = "dashed")
 
+plot(x[order(x)][1:N*.6], fx[order(x)][1:N*.6], type = "b", cex = .5, main = "Estimated f(x)[1:.6*N] with riemannian metric")
+abline(h = dentheta, col = "red", lty = "dashed")
+text(x = 0.7, y = 0.55, paste("f(theta) = ", round(dentheta[1], 3)), col = "red")
+
+summary(denx)
+summary(fxkde)
+summary(fx)
+
+# rank correlation
 cor(denx, fx, method = "s")
 cor(denx, fxkde, method = "s")
 
-hist(x, probability = T)
-
-
-
-# evaluate density on grid points
-fhat <- NULL
-for (k in 1:N) {
-  # hk <- h[,,k]
-  z <-  abind::abind(z, array(mvtnorm::dmvnorm(x = eval.points, mean = x[k,], sigma = hk), dim = gridsize), along = d + 1)  # stack array of dimension (gridsize*gridsize) with abind
-}
-z <- rowMeans(z, dims = 2, na.rm = TRUE)
-
-
-
-
-
-
-
-
+plot(rank(denx), rank(fx), main = paste("Rank correlation:", round(cor(rank(denx), rank(fx), method = "s"), 3)))
+plot(rank(denx), rank(fxkde), main = paste("Rank correlation:", round(cor(rank(denx), rank(fxkde), method = "s"), 3)))
 
 # # If x ~ U(0,1), theta = acos(x)
 # par(mfrow=c(1,2))
@@ -85,6 +73,9 @@ z <- rowMeans(z, dims = 2, na.rm = TRUE)
 # plot(theta, dentheta, main = "Polar CDF")
 
 
+
+
+# Now reduce the dimension and estimate the density of the embedding
 train <- data.frame(x = x, y = y)
 # Parameters fixed
 # x <- train
@@ -114,77 +105,126 @@ metric_isomap <- metricML(x = train, s = s, k = k, radius = 2, method = method, 
 ## -------------------------------------------------------------------------------
 # fixed bandwidth
 fn <- metric_isomap$embedding
-E1 <- fn[,1] # rename as Ed to match the aesthetics in plot_ellipse()
+# E1 <- fn[,1] # rename as Ed to match the aesthetics in plot_ellipse()
 xr <- diff(range(fn, na.rm=TRUE))
 xextend <- 0.15
-xr <- c(min(x)-xr*xextend,max(x)+xr*xextend)
+xr <- c(min(fn) - xr * xextend, max(fn) + xr * xextend)
 h <- hpi(fn, binned = TRUE) # 0.0767
 denks <- ks::kde(x = fn, h = h, xmin = xr[1], xmax = xr[2], gridsize = gridsize, eval.points = fn)
 str(denks)
-denfixed <- denks$estimate
-range(denfixed)
-range(denx)
-cor(denx, denfixed, method = "s") # -0.380
+ffixed <- denks$estimate
+summary(ffixed)
+summary(denx)
+cor(denx, ffixed, method = "s") # -0.380
+plot(fn, ffixed)
+# plot(theta, dentheta)
+plot(x, denx)
 
-Rn <- metric_isomap$rmetric
+rmetric <- metric_isomap$rmetric
 opt.method <- "SCALED"
 riem.scale <- 1
 # fisomap <- vkde(x = fn, h = Rn, gridsize = gridsize, eval.points = fn, opt.method = opt.method, riem.scale = riem.scale)
+adj_matrix <- metric_isomap$adj_matrix
+dim(adj_matrix)
+summary(metric_isomap)
+
+# geodesic distance estimation
+# Option 1: true geodesic distance d_g
+# Option 2: approximate geodesic distance with inner product adjusted by Riem metric (x-x_i)'h_i^(-1)(x-xi)
+# Option 3: approximate geodesic distance with shortest path graph distance d_G
+d <- 1
+fhat <- rep(0, N)
+h <- h
+for(i in 1:N){
+  hk <- rmetric[,,k]
+  # fi <- hk ^ (-1/2) * dnorm(x = acos(fn), mean = acos(fn[i]), sd = h) # true geodesic distance
+  fi <- dnorm(x = fn, mean = fn[i], sd = h * sqrt(hk)) # approximate geodesic distance with inner product adjusted by Riem metric
+  # fi <- hk ^ (-1/2) * h ^ (-1) * (2 * pi) ^ (-1/2) * exp(- adj_matrix[,i]^2 / (2 * h ^ 2)) # approximate geodesic distance with shortest path graph distance
+  fhat <- fhat + fi
+}
+fhat <- fhat / N
+plot(fn, fhat)
+abline(h = dentheta, col = "red", lty = "dashed")
+text(x = 0.5, y = 0.5, paste("f(theta) = ", round(dentheta[1], 3)), col = "red")
+summary(fhat)
+cor(denx, fhat, method = "s")
+
+plot(fn, ffixed)
+abline(h = dentheta, col = "red", lty = "dashed")
+text(x = 0.5, y = 0.5, paste("f(theta) = ", round(dentheta[1], 3)), col = "red")
+summary(ffixed)
+
+
+# How to compare rank of density estimates???
+cor(denx, fhat, method = "s")
+cor(denx, ffixed, method = "s")
+plot(rank(denx), rank(fhat), main = paste("Rank correlation:", round(cor(rank(denx), rank(fhat), method = "s"), 3)))
+plot(rank(denx), rank(ffixed), main = paste("Rank correlation:", round(cor(rank(denx), rank(ffixed), method = "s"), 3)))
+
+
+mean((fhat - dentheta)^2)
+mean((ffixed - dentheta)^2)
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-# E2 <- fn[,2]
-# prob <- c(1, 50, 95, 99) # c(1, 10, 50, 95, 99) #
-# p_hdr_isomap <- hdrscatterplot_new(E1, kde.package = "ks", levels = prob, noutliers = noutliers, label = NULL)
-# p_hdr_isomap_p <- p_hdr_isomap$p +
-#   plot_ellipse(metric_isomap, add = T, ell.no = 50, ell.size = 0,
-#                color = blues9[5], fill = blues9[1], alpha = 0.2)
-# # p_hdr_isomap
-# h_hdr_isomap <- p_hdr_isomap$den$den$h
-
-## ----outliers-------------------------------------------------------------------
-Rn <- metric_isomap$rmetric # array
-# fisomap <- vkde2d(x = fn[,1], y = fn[,2], h = Rn*riem.scale, gridsize = gridsize) # $x $y $z
-# fxy_isomap <- hdrcde:::interp.2d(fisomap$x, fisomap$y, fisomap$z, x0 = E1, y0 = E2)
-# plot_contour(metric_isomap, gridsize = gridsize, riem.scale = riem.scale) # estimate grid densities with vkde()
-
-opt.method <- c("AMISE", "MEAN", "SCALED")[2]
-riem.scale <- .1
-fisomap <- vkde(x = fn, h = Rn, gridsize = gridsize, eval.points = fn, opt.method = opt.method, riem.scale = riem.scale) # 0.923
-# fisomap <- vkde(x = fn, h = Rn*riem.scale, gridsize = gridsize, eval.points = fn) # 0.64
-# # if scaling the bandwidth
-# fisomap_MEAN <- vkde(x = fn, h = Rn, gridsize = gridsize, eval.points = fn, opt.method = "MEAN") # 0.964
-# fisomap_AMISE <- vkde(x = fn, h = Rn, gridsize = gridsize, eval.points = fn, opt.method = "AMISE") # 0.941
-# h_isomap <- fisomap$H
-# str(fisomap)
-# summary(fisomap$estimate)
-# all.equal(fisomap$estimate, p_isomap$densities)
-
-# check if vkde with grid estimate is the same as hdrcde::interp.2d
-# fixgrid_isomap <- vkde(x = fn, h = NULL, gridsize = gridsize)
-# summary(fixgrid_isomap)
-# interpden_fix <- hdrcde:::interp.2d(fixgrid_isomap$eval.points[[1]], fixgrid_isomap$eval.points[[2]], fixgrid_isomap$estimate, x0 = E1, y0 = E2)
-# all.equal(interpden_fix, fisomap$estimate)
-
-## ----hdroutliers----------------------------------------------------------------
-p_isomap <- plot_outlier(x = metric_isomap, gridsize = gridsize, prob = prob, riem.scale = riem.scale, f = fisomap, ell.size = 0)
-# all.equal(fxy_isomap, p_isomap$densities)
-
-## ----compoutlier, eval = FALSE--------------------------------------------------
-(p_isomap$p + p_hdr_isomap$p) + coord_fixed() + 
-  plot_annotation(title = "Left: variable bandwidth; Right: fixed bandwidth", theme = theme(plot.title = element_text(hjust = 0.5)))
-
-cormethod <- c("pearson", "kendall", "spearman")[3]
-cor(preswissroll$den, p_isomap$densities, method = cormethod)
-cor(preswissroll$den, p_hdr_isomap$densities, method = cormethod)
+# # # evaluate density on grid points
+# # d <- 1
+# # z <- NULL
+# # rmetric <- metric_isomap$rmetric
+# # for (k in 1:N) {
+# #   hk <- as.matrix(rmetric[,,k])
+# #   z <-  abind::abind(z, array(h ^ (- d / 2) * mvtnorm::dmvnorm(x = fn, mean = fn[k,], sigma = h * hk), dim = rep(N, d)), along = d + 1)  # stack array of dimension (gridsize*gridsize) with abind
+# # }
+# # # fhat <- rowMeans(z, dims = 2, na.rm = TRUE)
+# # fhat <- rowMeans(z, na.rm = TRUE)
+# # plot(fn, fhat)
+# 
+# 
+# 
+# 
+# # E2 <- fn[,2]
+# # prob <- c(1, 50, 95, 99) # c(1, 10, 50, 95, 99) #
+# # p_hdr_isomap <- hdrscatterplot_new(E1, kde.package = "ks", levels = prob, noutliers = noutliers, label = NULL)
+# # p_hdr_isomap_p <- p_hdr_isomap$p +
+# #   plot_ellipse(metric_isomap, add = T, ell.no = 50, ell.size = 0,
+# #                color = blues9[5], fill = blues9[1], alpha = 0.2)
+# # # p_hdr_isomap
+# # h_hdr_isomap <- p_hdr_isomap$den$den$h
+# 
+# ## ----outliers-------------------------------------------------------------------
+# Rn <- metric_isomap$rmetric # array
+# # fisomap <- vkde2d(x = fn[,1], y = fn[,2], h = Rn*riem.scale, gridsize = gridsize) # $x $y $z
+# # fxy_isomap <- hdrcde:::interp.2d(fisomap$x, fisomap$y, fisomap$z, x0 = E1, y0 = E2)
+# # plot_contour(metric_isomap, gridsize = gridsize, riem.scale = riem.scale) # estimate grid densities with vkde()
+# 
+# opt.method <- c("AMISE", "MEAN", "SCALED")[2]
+# riem.scale <- .1
+# fisomap <- vkde(x = fn, h = Rn, gridsize = gridsize, eval.points = fn, opt.method = opt.method, riem.scale = riem.scale) # 0.923
+# # fisomap <- vkde(x = fn, h = Rn*riem.scale, gridsize = gridsize, eval.points = fn) # 0.64
+# # # if scaling the bandwidth
+# # fisomap_MEAN <- vkde(x = fn, h = Rn, gridsize = gridsize, eval.points = fn, opt.method = "MEAN") # 0.964
+# # fisomap_AMISE <- vkde(x = fn, h = Rn, gridsize = gridsize, eval.points = fn, opt.method = "AMISE") # 0.941
+# # h_isomap <- fisomap$H
+# # str(fisomap)
+# # summary(fisomap$estimate)
+# # all.equal(fisomap$estimate, p_isomap$densities)
+# 
+# # check if vkde with grid estimate is the same as hdrcde::interp.2d
+# # fixgrid_isomap <- vkde(x = fn, h = NULL, gridsize = gridsize)
+# # summary(fixgrid_isomap)
+# # interpden_fix <- hdrcde:::interp.2d(fixgrid_isomap$eval.points[[1]], fixgrid_isomap$eval.points[[2]], fixgrid_isomap$estimate, x0 = E1, y0 = E2)
+# # all.equal(interpden_fix, fisomap$estimate)
+# 
+# ## ----hdroutliers----------------------------------------------------------------
+# p_isomap <- plot_outlier(x = metric_isomap, gridsize = gridsize, prob = prob, riem.scale = riem.scale, f = fisomap, ell.size = 0)
+# # all.equal(fxy_isomap, p_isomap$densities)
+# 
+# ## ----compoutlier, eval = FALSE--------------------------------------------------
+# (p_isomap$p + p_hdr_isomap$p) + coord_fixed() + 
+#   plot_annotation(title = "Left: variable bandwidth; Right: fixed bandwidth", theme = theme(plot.title = element_text(hjust = 0.5)))
+# 
+# cormethod <- c("pearson", "kendall", "spearman")[3]
+# cor(preswissroll$den, p_isomap$densities, method = cormethod)
+# cor(preswissroll$den, p_hdr_isomap$densities, method = cormethod)
