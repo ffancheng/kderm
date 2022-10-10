f37efdfde9b9834abee220ea80753a1d50c28961
Fan Cheng <fan.cheng@monash.edu>
2022-10-11 03:26:55 +1100

    Plots all updated for simulations
    Table 3 last line has empty line before when knitted from Rmd but not from tex

--- R/Simulation4d_03plots.R
+++ R/Simulation4d_03plots.R
@@ -17,7 +17,12 @@ methods <- c("isomap", "lle", "le", "umap")
 # load HPC results for four methods
 files <- list.files(path = "data/truedenk100", pattern = "*.rda", full.names = TRUE)
 lapply(files, load, .GlobalEnv)
-
+f <- tibble(fxy = fxy,
+            fxy_vkde_isomap = fisomap$estimate, fxy_hdr_isomap = fixden_isomap$estimate,
+            fxy_vkde_lle = flle$estimate, fxy_hdr_lle = fixden_lle$estimate,
+            fxy_vkde_le = fle$estimate, fxy_hdr_le = fixden_le$estimate,
+            fxy_vkde_umap = fumap$estimate, fxy_hdr_umap = fixden_umap$estimate
+)
 
 ###--------------------------------------------------------
 ## 2. Table for density correlations
@@ -42,23 +47,67 @@ cors %>%
 
 
 ###--------------------------------------------------------
-## 2. Table for correctlly identified outliers percentage
+## 2.2 Table for correctlly identified outliers percentage
 ###--------------------------------------------------------
-
-
+prob <- c(1, 10, 50, 95, 99)
+hdrbreaks <- c(0, quantile(1:N, c(sort(100 - prob) / 100)), N)
+hdrlabels <- c(">99", paste0(100 - prob)) #, "%", not working for latex, \\%
+frankhdr <- f %>% 
+  summarise_all(.fun = list(~ rank(x = ., ties.method = "average"))) %>% # using average ties
+  # mutate(outliers = factor(label, levels = c("TRUE", "FALSE")),
+  #        dist2center = dist2center) %>%
+  `colnames<-`(gsub("fxy_", "", colnames(.))) %>% 
+  pivot_longer(cols = -c(fxy), names_to = "kde", values_to = "densities") %>% 
+  separate(kde, c("KDE", "ML"), sep = "_") %>% 
+  mutate(KDE = factor(KDE, levels = c("vkde", "hdr"), labels = c("DC-KDE", "KDE")),
+         ML = factor(ML, levels = methods, labels = c("ISOMAP", "LLE", "Laplacian Eigenmaps", "UMAP"))
+  ) %>% 
+  mutate(truehdr = cut(fxy, 
+                       breaks = hdrbreaks, 
+                       labels = hdrlabels),
+         esthdr = cut(densities, 
+                      breaks = hdrbreaks, 
+                      labels = hdrlabels)
+         ) %>% 
+  mutate(correcthdr = (truehdr == esthdr))
+hdrtable <- frankhdr %>% 
+  group_by(KDE, ML, truehdr) %>% 
+  summarise(numhdr = sum(correcthdr) / n(), .groups = "drop") %>% 
+  pivot_wider(names_from = c(KDE, ML), values_from = numhdr) %>% 
+  column_to_rownames(var = "truehdr") %>%
+  `colnames<-` (NULL)
+hdrmax <- (as.matrix(hdrtable[, 1:4]) - as.matrix(hdrtable[, 5:8])) >= 0
+hdrmax <- cbind(hdrmax, !hdrmax)[, c(1, 5, 2, 6, 3, 7, 4, 8)] %>% `rownames<-` (NULL)
+hdrtable <- hdrtable[, c(1, 5, 2, 6, 3, 7, 4, 8)]
+hdrtable %>% 
+  kableExtra::kbl(caption = "Percentage comparison of correct highest density regions in density estimation of four manifold learning embeddings.", booktabs = TRUE, digits = 3, escape = FALSE, align = "c",
+                  # col.names = c(rep(c("DC-KDE", "KDE"), 4))
+                  ) %>%
+  kable_classic("striped") %>% 
+  kable_styling(latex_options = "scale_down") %>%
+  kable_paper(full_width = TRUE) %>%
+  add_header_above(c("HDRs" = 1, rep(c("DC-KDE" = 1, "KDE" = 1), 4))) %>%
+  add_header_above(c(" " = 1, "ISOMAP" = 2, "LLE" = 2, "Laplacian Eigenmaps" = 2, "UMAP" = 2)) %>% 
+  # column_spec(1, bold = TRUE) %>% 
+  # add_indent(1:6) %>% 
+  column_spec(2, bold = hdrmax[,1]) %>% 
+  column_spec(3, bold = hdrmax[,2]) %>% 
+  column_spec(4, bold = hdrmax[,3]) %>% 
+  column_spec(5, bold = hdrmax[,4]) %>% 
+  column_spec(6, bold = hdrmax[,5]) %>% 
+  column_spec(7, bold = hdrmax[,6]) %>% 
+  column_spec(8, bold = hdrmax[,7]) %>% 
+  column_spec(2*(1:4), border_left = T, background = grey(0.9))
+  
+  
+# save(frankhdr, hdrtable, hdrmax, file = paste0("paper/figures/hdrtable_", "4d_N", N, "_4ml_radius", 10, "_r", 1, ".rda"))
 
 
 ###--------------------------------------------------------
 ## 3. Rank comparison plot, highlight outliers
 ###--------------------------------------------------------
-f <- tibble(fxy = fxy,
-            fxy_vkde_isomap = fisomap$estimate, fxy_hdr_isomap = fixden_isomap$estimate,
-            fxy_vkde_lle = flle$estimate, fxy_hdr_lle = fixden_lle$estimate,
-            fxy_vkde_le = fle$estimate, fxy_hdr_le = fixden_le$estimate,
-            fxy_vkde_umap = fumap$estimate, fxy_hdr_umap = fixden_umap$estimate
-            )
 frank <- f %>% 
-  summarise_all(.fun = list(~ rank(x = ., ties.method = "random"))) %>% 
+  summarise_all(.fun = list(~ rank(x = ., ties.method = "random"))) %>% # random tie method for better visualization (not verticle lines in >99% region)
   mutate(outliers = factor(label, levels = c("TRUE", "FALSE")),
          dist2center = dist2center)
 
@@ -69,6 +118,8 @@ frank_long <- frank %>%
   mutate(KDE = factor(KDE, levels = c("vkde", "hdr"), labels = c("DC-KDE", "KDE")),
          ML = factor(ML, levels = methods, labels = c("ISOMAP", "LLE", "Laplacian Eigenmaps", "UMAP"))
   )
+
+
 p_rank <- frank_long %>%
   ggplot(aes(fxy, densities, col = dist2center)) +
   geom_rect(xmin = 0, xmax = log10(N / 100), ymin = 0, ymax = log10(N / 100), fill = "#B7B7B7", col = "#B7B7B7") +
@@ -77,18 +128,13 @@ p_rank <- frank_long %>%
   scale_size_manual(values = c(`TRUE` = 2, `FALSE` = 1)) +
   scale_shape_manual(values = c(`TRUE` = 17, `FALSE` = 20)) +
   scale_alpha_manual(values = c(`TRUE` = 1, `FALSE` = .2) ) + 
-  # scale_color_manual(
-  #   name = "HDRs",
-  #   breaks = levels(frank$col),
-  #   values = rev(c(RColorBrewer::brewer.pal(2, "YlOrRd")[-1], "#000000"))[1:2],
-  #   labels = paste0(levels(frank$col), "%")
-  # )
   scale_color_viridis(option = "inferno", direction = -1) +
   labs(x = "True density rank", y = "Estimated density rank", col = "Distance", size = "Outliers", shape = "Outliers", alpha = "Outliers") +
   scale_x_log10() + 
   scale_y_log10() + 
   theme(legend.position = "bottom")
 # ggsave(paste0("paper/figures/", "sim4d", N, "_density_comparison_4ml_radius", 10, "_k200_rankdensity_circleoutlier_with1rec.png"), p_rank, width = 8, height = 10, dpi = 300)
+
 
 # Not run
 # ## scatterplot to compare f_xy for ISOMAP
@@ -131,13 +177,13 @@ p_rank <- frank_long %>%
 #   ggplot(aes(x = fxy, y = fxy_vkde, col = dist2center, shape = label)) +
 #   # scale_color_viridis(option = "A", direction = -1) +
 #   geom_point() +
-#   labs(x = "True density", y = "Laplacian Eigenmaps", color = "Distance", shape = "Kernels", title = "") +
+#   labs(x = "", y = "Laplacian Eigenmaps", color = "Distance", shape = "Kernels", title = "") +
 #   scale_y_continuous(limits = c(0, max(f$fxy_vkde)), n.breaks = 6)
 # pf_hdr <- f %>%
 #   ggplot(aes(x = fxy, y = fxy_hdr, col = dist2center, shape = label)) +
 #   # scale_color_viridis(option = "A", direction = -1) +
 #   geom_point() +
-#   labs(x = "True density", y = "", color = "Distance", shape = "Kernels", title = "") +
+#   labs(x = "", y = "", color = "Distance", shape = "Kernels", title = "") +
 #   scale_y_continuous(limits = c(0, max(f$fxy_hdr)), n.breaks = 6)
 # p2 <- pf_vkde + pf_hdr
 # 
